45ac37fe3e14d04072cae39d93d99ad6e7b3d199
AbhyasKanaujia <k.abhyas@gmail.com>
2021-11-18 23:13:42 +0530

    add delete animation and modal to note

--- frontend/src/components/Note.js
+++ frontend/src/components/Note.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Button, Card, Popup } from "semantic-ui-react";
+import { Button, Card, Modal, Popup, Transition } from "semantic-ui-react";
 import { Link } from "react-router-dom";
 import axios from "axios";
 import ErrorView from "./ErrorView";
@@ -8,6 +8,7 @@ const Note = ({ id, title, content }) => {
   const [error, setError] = useState(null);
   const [deleted, setDeleted] = useState(false);
   const [deleting, setDeleting] = useState(false);
+  const [open, setOpen] = useState(false);
 
   const deleteNote = async () => {
     try {
@@ -22,9 +23,9 @@ const Note = ({ id, title, content }) => {
   };
 
   const extra = (
-    <>
-      <Button.Group floated="right">
-        <Button as={Link} to={`/edit/${id}`} icon="pencil" basic />
+    <Button.Group floated="right">
+      <Button as={Link} to={`/edit/${id}`} icon="pencil" basic />
+      <Transition visible={!deleted} unmountOnHide>
         <Popup
           trigger={<Button icon="trash" basic />}
           on="click"
@@ -37,24 +38,28 @@ const Note = ({ id, title, content }) => {
             />
           }
         />
-      </Button.Group>
-    </>
+      </Transition>
+    </Button.Group>
   );
 
   const getNoteCard = () => {
-    return <Card header={title} description={content} extra={extra} />;
+    return (
+      <Transition visible={!deleted} unmountOnHide>
+        <Modal
+          onClose={() => setOpen(false)}
+          onOpen={() => setOpen(true)}
+          trigger={<Card header={title} description={content} extra={extra} />}
+          header={title}
+          content={content}
+          actions={[
+            { content: "Edit" },
+            { content: "Delete", negative: true, onClick: () => deleteNote() },
+          ]}
+        />
+      </Transition>
+    );
   };
-  return (
-    <>
-      {error ? (
-        <ErrorView message={error.message} />
-      ) : !deleted ? (
-        getNoteCard()
-      ) : (
-        <></>
-      )}
-    </>
-  );
+  return <>{error ? <ErrorView message={error.message} /> : getNoteCard()}</>;
 };
 
 export default Note;
